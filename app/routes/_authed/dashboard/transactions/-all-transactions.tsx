bbb0bff83df4dd984f606f96ad192f5c46c79145
boyfromthewell <tnsdyd10@naver.com>
2025-05-09 17:25:31 +0900

    feat: 거래 내역 table UI

--- app/routes/_authed/dashboard/transactions/-all-transactions.tsx
+++ app/routes/_authed/dashboard/transactions/-all-transactions.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -7,19 +8,38 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Link } from "@tanstack/react-router";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import { useState } from "react";
+import numeral from "numeral";
+import { PencilIcon } from "lucide-react";
 
 export default function AllTransactions({
   yearsRange,
   month,
   year,
+  transactions,
 }: {
   yearsRange: number[];
   month: number;
   year: number;
+  transactions: {
+    id: number;
+    description: string;
+    amount: string;
+    category: string | null;
+    transactionType: "수입" | "지출" | null;
+    transactionDate: string;
+  }[];
 }) {
   const [selectedYear, setSelectedYear] = useState(year);
   const [selectedMonth, setSelectedMonth] = useState(month);
@@ -84,6 +104,66 @@ export default function AllTransactions({
         <Button asChild>
           <Link to="/dashboard/transactions/new">새 거래 등록</Link>
         </Button>
+        {!transactions.length && (
+          <p className="text-center py-10 text-lg text-muted-foreground">
+            거래 내역이 없습니다.
+          </p>
+        )}
+        {!!transactions.length && (
+          <Table className="mt-4">
+            <TableHeader>
+              <TableRow>
+                <TableHead>거래 일시</TableHead>
+                <TableHead>설명</TableHead>
+                <TableHead>거래 유형</TableHead>
+                <TableHead>카테고리</TableHead>
+                <TableHead>총액</TableHead>
+                <TableHead />
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {transactions.map(
+                ({
+                  id,
+                  transactionDate,
+                  description,
+                  transactionType,
+                  category,
+                  amount,
+                }) => (
+                  <TableRow key={id}>
+                    <TableCell>
+                      {format(transactionDate, "MMM do", { locale: ko })}
+                    </TableCell>
+                    <TableCell>{description}</TableCell>
+                    <TableCell>
+                      <Badge
+                        className={
+                          transactionType === "수입"
+                            ? "bg-lime-500"
+                            : "bg-orange-500"
+                        }
+                      >
+                        {transactionType}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{category}</TableCell>
+                    <TableCell>₩{numeral(amount).format("0,0[.]00")}</TableCell>
+                    <TableCell className="text-right">
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        aria-label="거래 수정"
+                      >
+                        <PencilIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        )}
       </CardContent>
     </Card>
   );
